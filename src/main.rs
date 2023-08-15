c0efcf7971276067c23e646a9bd0e740edf339d6
Adol Rodriguez <adolsalamanca@gmail.com>
2023-08-15 14:00:07 +0200

    add code to generate random obstacles

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use crate::domain::space::Map;
+use crate::domain::space::{Map, Point};
 use std::io;
 use std::process::exit;
-
+use rand::Rng;
 mod domain;
 
 fn main() {
@@ -12,7 +12,22 @@ fn main() {
     println!("Enter planet width:");
     let width:i64 = read_number(&mut input);
 
-    let p = Map::new(length, width, vec![]);
+    println!("Enter number of obstacles:");
+    let number_of_obstacles:i64 = read_number(&mut input);
+    if number_of_obstacles < 0 {
+        eprintln!("error: obstacles need to be equal or higher than 0");
+        return
+    }
+
+    let mut obstacles:Vec<Point> = Vec::new();
+    let mut random_generator = rand::thread_rng();
+    for _ in 0..number_of_obstacles {
+        let x = random_generator.gen_range(-width..width+1);
+        let y = random_generator.gen_range(-length..length+1);
+        obstacles.push(Point::new(x,y))
+    }
+
+    let p = Map::new(length, width, obstacles);
     println!("{:?}", p)
 }
 
